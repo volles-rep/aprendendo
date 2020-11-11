59fb52117df5534f1499aceb30384d3d573cf42d
volles-rep <74315696+volles-rep@users.noreply.github.com>
2020-11-11 15:59:50 -0300

    v1

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import { useState} from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h3>contador a refresh automatico.</h3>
+            <Contador/>
+        </div>)
+}
+
+function Contador() {
+    const [contador,setContador] = useState(1);
+
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adicionar</button>
+        </div>
+    )
+}
+export default Home
